1f628a523eeb5d3a19b9b56f3ad17026b5d8d3c8
Med <mohamed.boucadair@orange.com>
2026-03-09 15:09:09 +0100

    Update yang.mk

--- yang.mk
+++ yang.mk
@@ -56,9 +56,4 @@ yang-clean:
 FORCE:
 
 $(YANGDIR)/trees/%.tree: $(YANGDIR)/%.yang $(YANGDIR)/trees FORCE
-	@output=$$(pyang $(OPTIONS) -p $(YANG_PATH) $< ); \
-        if [ -n "$$output" ]; then \
-          echo "$$output" > $@; \
-        else \
-          echo "Skipping empty tree $<" >&2; \
-        fi
+	pyang $(PYANG_OPTIONS) -p $(YANG_PATH) $< > $@
